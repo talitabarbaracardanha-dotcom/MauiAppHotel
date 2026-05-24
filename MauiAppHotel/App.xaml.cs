a01060bf7c4281ce51780ba626236293ff82ab7a
Talita Lopes <talitabc.lopes@hotmail.com>
2026-05-24 14:10:02 -0300

    Add recursos aos aplicativo

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppHotel.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiarioAdulto = 110.0,
+                ValorDiarioCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiarioAdulto = 80.0,
+                ValorDiarioCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiarioAdulto = 50.0,
+                ValorDiarioCrianca = 25.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiarioAdulto = 25.0,
+                ValorDiarioCrianca = 12.5
+            }
+        };
+
         public App()
         {
             InitializeComponent();
