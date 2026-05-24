2bfabc5f1302ef87c03a0829daa5df074efea6a5
Talita Lopes <talitabc.lopes@hotmail.com>
2026-05-24 18:04:55 -0300

    add recursos ao app

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,16 +1,19 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
-	App PropriedadesApp;
+     App PropriedadesApp;
+    
 
-	public ContratacaoHospedagem()
+    public ContratacaoHospedagem()
 	{
 		InitializeComponent();
 
 		PropriedadesApp = (App)Application.Current;
 
-		pck_quatro.ItemsSource = PropriedadesApp.lista_quartos;
+		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
 
 		dtpck_checkin.MinimumDate = DateTime.Now;
 		dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
@@ -19,21 +22,35 @@ public partial class ContratacaoHospedagem : ContentPage
 		dtpck_checkout.MaximumDate = dtpck_checkin.Date.Value.AddMonths(6);
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
 		try 
 		{
-			Navigation.PushAsync(new HospedagemContratada());
+			Hospedagem h = new Hospedagem
+			{
+				QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+				QntAdultos = Convert.ToInt32(stp_adultos.Value),
+				QntCriancas = Convert.ToInt32(stp_criancas.Value),
+				DataCheckin = dtpck_checkin.Date.Value,
+				DataCheckout = dtpck_checkout.Date.Value,
+			};
+
+
+			await Navigation.PushAsync(new HospedagemContratada()
+			{
+				BindingContext = h
+			});
+
 		
 		}catch (Exception ex)
 		{
-			DisplayAlert("Ops", ex.Message, "OK");
+			await DisplayAlert("Ops", ex.Message, "OK");
 		}
     }
 
     private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
     {
-		DatePicker elemento = sender as DatePicker;
+		DatePicker elemento = (DatePicker)sender;
 
 		DateTime data_selecionada_checkin = elemento.Date.Value;
 
